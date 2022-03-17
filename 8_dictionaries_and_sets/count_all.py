49b7eaed878955cd93fca81e355678b5efe3085f
nyckolas <nyckolas.work@gmail.com>
2022-03-17 17:29:36 +0200

    Modified coun_all.py

--- 8_dictionaries_and_sets/count_all.py
+++ 8_dictionaries_and_sets/count_all.py
@@ -3,3 +3,8 @@ from collections import Counter
 def count_all(alist):
 	result = dict(Counter(alist))
 	return print(result)
+
+# Test programm
+count_all(['cat', 'dog', 'cat'])
+count_all('hello')
+count_all('*' * 20)
