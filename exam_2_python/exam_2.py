edc7c4874e81ae424da22620ca03734c10d588fb
nyckolas <nyckolas.work@gmail.com>
2022-03-19 21:32:36 +0200

    Update exam_2.py

--- exam_2_python/exam_2.py
+++ exam_2_python/exam_2.py
@@ -13,11 +13,69 @@ def build_query_string(param):
 		result =''.join(str(i)+str(param[i])) + result
 	return print(result)
 
+def to_roman(num):
+	roman = { # Dictionary of roman numbers
+		1000: 'M',
+		900: 'CM',
+		500: 'D',
+		400: 'CD',
+		100: 'C',
+		90: 'XC',
+		50: 'L',
+		40: 'XL',
+		10: 'X',
+		9: 'IX',
+		5: 'V',
+		4: 'IV',
+		1: 'I'
+		}
+	result = ''
+	if num < 4000:	
+		for i in roman:
+			result += num // i * roman[i]
+			num %= i
+		return print(result)
+	else:
+		return print(False)
+
+def to_arabic(txt):
+	roman = { # Dictionary of roman numbers
+		3000: 'M',
+		2000: 'M',
+		1000: 'M',
+		900: 'CM',
+		500: 'D',
+		400: 'CD',
+		100: 'C',
+		90: 'XC',
+		50: 'L',
+		40: 'XL',
+		10: 'X',
+		9: 'IX',
+		5: 'V',
+		4: 'IV',
+		1: 'I'
+		}
+	result = 0
+	txt = txt.upper()
+	for i in roman:
+		while txt.startswith(roman[i]):
+			result += i
+			txt = txt[len(roman[i]):]
+			print(txt[len(roman[i]):])
+			print(txt)
+			#print(roman[i])
+			#print(i)
+	if txt == '':
+		return print(result)
+	else: return print(False)
+
 
 def test():
 
-	param = {'per': 10, 'page': 1}
-	build_query_string(param)
+	num = 3999
+	to_roman(num)
+	to_arabic('VVVV')
 
 
 if __name__ == '__main__':
