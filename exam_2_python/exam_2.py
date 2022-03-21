e7fe3f63e21909b256fe92b183d3660cfc33fe5a
nyckolas <nyckolas.work@gmail.com>
2022-03-21 16:43:23 +0200

    Update exam_2.py

--- exam_2_python/exam_2.py
+++ exam_2_python/exam_2.py
@@ -86,15 +86,31 @@ def merged(*alist):
 			#print(dict(x)) # выводим на экран каждую итерацию
 	print(dict(x))
 	return dict(x)
+
+def gen_diff(d1, d2):
+	res = defaultdict(str)
+	for k in d1:
+		if k not in d2:
+			res[k] = 'deleted'
+		elif k in d2 and d1[k]!=d2[k]:
+			res[k] = 'changed'
+		elif k in d2 and d1[k]==d2[k]:
+			res[k] = 'unchanged'
+		elif k in d2 and k not in d1:
+			res[k] = 'added'
+	for k in d2:
+		if k not in d1:
+			res[k] = 'added'
+	print(dict(res))
+	return res
 			
 
 def test():
 
-	print(merged({}, {}) == {}) # True
-	print(merged(
-		{'a': 1, 'b': 2},
-		{'b': 10, 'c': 100}
-	) == {'a': {1}, 'b': {2, 10}, 'c': {100}}) # True
+	gen_diff(
+		{"one": "eon", "two": "two", "four": True},
+		{"two": "own", "zero": 4, "four": True},
+		) # {"one": "deleted", "two": "changed", "four": "unchanged", "zero": "added"}
 
 
 if __name__ == '__main__':
