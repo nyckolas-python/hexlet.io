d60ab1d4f0d8a5da4e3f4d12c6551a9e9f4c15ac
nyckolas <nyckolas.work@gmail.com>
2022-03-25 16:07:23 +0200

    Add partial_apply.py

---
+++ 9_map_filter_reduce/partial_apply.py
@@ -0,0 +1,12 @@
+def partial_apply(function, name):
+	
+	def inner(surname):
+		return function(name, surname)
+	return inner
+
+def greet(name, surname):
+	res = 'Hello, {name} {surname}!'.format(name=name, surname=surname)
+	return print(res)
+
+f = partial_apply(greet, 'Dorian')
+f('Gray') # function(name, inner)
