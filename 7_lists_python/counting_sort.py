f026dac1f43f5ef1cabf176d61ca861447d114a1
nyckolas <nyckolas.work@gmail.com>
2022-03-26 18:00:57 +0200

    Update counting_sort.py

--- 7_lists_python/counting_sort.py
+++ 7_lists_python/counting_sort.py
@@ -3,10 +3,30 @@ def counting_sort():
 	count = [0]*(max(a)+1)
 	for i in a:
 		count[i]+=1
-	#print(count)
-	print(a)
-	for i in range(max(a)+1):
-		if count[i]>0:
-			print((str(i)+', ')*count[i], end='')
+		print(i, end=' ') # вывод неотсортированного списка чисел
+	print()
 
-counting_sort()
+
+	for i in range(max(a)+1):
+		if count[i]>0: # выводить только если количество 1 и больше
+			print((str(i)+' ')*count[i], end='') # вывод ОТСОРТИРОВАННОГО списка чисел
+
+def text_counting_sort():
+	a = 'abcdmvkefnvknenmrvjlkKLJKLJ"KJklmlrkeljeklmkl'
+	count = [0]*26 # количество букв в анг. алфивите
+	
+	for i in a.lower():
+		if i>='a' and i<='z':
+			nomer = ord(i)-97
+			count[nomer] += 1
+			print(i, end=' ') # вывод неотсортированной строки
+	print()
+	for i in range(26):
+		if count[i]>0: # выводить только если количество 1 и больше
+			
+			print((chr(i+97)+' ')*count[i], end='') # вывод ОТСОРТИРОВАННОЙ строки
+			#print(chr(i+97), count[i], end=' \n') # вывод БУКВА КОЛТЧЕСТВО
+		
+counting_sort() # 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4
+print('')
+text_counting_sort()
