31bc66e8cc9ee5b01a52877ae3fb6d3ba8512fef
nyckolas <nyckolas.work@gmail.com>
2022-03-29 13:36:56 +0300

    Add memoizing.py

---
+++ 9_map_filter_reduce/memoizing.py
@@ -0,0 +1,34 @@
+cache = {} # словарь с кешем
+			
+def memoizing(count):
+	def memoized(function):	
+		def inner(*args, **kwargs):
+			if args[0] in cache: # Проверяем присутствие в словаре аргумент ф-ции
+				res = cache[args[0]] # В результате присваиваем значение по ключу аргумента
+				print(f'We have in cache: {res}') # Выводи результат с помощья ф-строки
+				return res
+			else: # Если аргумента нет в словаре
+				res = function(*args, **kwargs)
+				while len(cache) >= count: # Віполняем если словарь больше или равно count
+					first_key = list(cache.keys())[0] # Берём ключ первого элемента словаря
+					cache.pop(first_key) # Удаляем элемент словаря по ключу
+				else:
+					cache[args[0]] = res # добавляем в словарь наш аргумент и результат
+					print(res) # Выводим выполняние ф-ции
+					return res
+		return inner
+	return memoized	
+
+# Test Program
+@memoizing(3)
+def f(x):
+	print('Calculating...')
+	return x*10
+
+f(1) # Calculating... 10
+f(1) # We have in cache: 10
+f(2) # Calculating... 20
+f(3) # Calculating... 30
+f(4) # Calculating... 40
+f(1) # Calculating... 10
+print(cache) # {3: 30, 4: 40, 1: 10}
