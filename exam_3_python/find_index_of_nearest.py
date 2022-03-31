6f6e1e2e8859f37bef7e193be029dab84a0b1fd5
nyckolas <nyckolas.work@gmail.com>
2022-03-31 17:57:19 +0300

    Add find_index_of_nearest.py

---
+++ exam_3_python/find_index_of_nearest.py
@@ -0,0 +1,22 @@
+
+from operator import itemgetter
+
+
+def find_index_of_nearest(n, alist):
+	if len(alist) > 0:
+		minlist = list(map(lambda x: abs(x-n), alist))
+		index = min(enumerate(minlist), key=itemgetter(1))[0]
+		#print(minlist)
+		print(index)
+		return index
+	else:
+		print(None) 
+		return None
+find_index_of_nearest(2, []) is None
+# True
+find_index_of_nearest(0, [15, 10, 3, 4])
+# 2
+find_index_of_nearest(4, [7, 5, 3, 2])
+# 1
+find_index_of_nearest(4, [7, 5, 4, 4, 3])
+# 2
