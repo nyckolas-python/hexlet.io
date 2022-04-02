5e52604607561c53bf367d8ed5985949edc206a0
nyckolas <nyckolas.work@gmail.com>
2022-04-02 08:42:18 +0300

    Update exam_3.py

--- exam_3_python/exam_3.py
+++ exam_3_python/exam_3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from ipaddress import IPv4Address
 from operator import itemgetter
 from typing import Counter
 
@@ -122,14 +123,49 @@ def hex2rgb(line):
 	return rgb
 
 
+def ip2int(line):
+	# First Method
+	adr = IPv4Address(line)
+	print(int(adr))
+
+	# Second Method
+	a,b,c,d = line.split('.')
+	x = int(a) * 256**3 + int(b) * 256**2 + int(c) * 256 + int(d)	
+	if x == int(adr):
+		print(x)
+		return x
+
+def int2ip(ipnum):
+	# First Method
+	line = str(IPv4Address(ipnum))
+	print(line)
+
+	#Second Method
+	a = ipnum//256**3
+	b = (ipnum - (a*256**3))//256**2
+	c = (ipnum - a*256**3 - b*256**2)//256
+	d = (ipnum - a*256**3 - b*256**2 - c*256)
+	res_line = f'{a}.{b}.{c}.{d}'
+	if res_line == line:
+		print(res_line)
+		return line
+
+
 def test():
 
-	rgb2hex(36, 171, 0);
-	# '#24ab00'
-	rgb2hex(r=36, g=171, b=0)
-	# '#24ab00'
-	hex2rgb('#24ab00')
-	# {'r': 36, 'g': 171, 'b': 0}
+	ip2int('128.32.10.1')
+	# 2149583361
+	ip2int('0.0.0.0')
+	# 0
+	ip2int('255.255.255.255')
+	# 4294967295
+
+	int2ip(2149583361)
+	# '128.32.10.1'
+	int2ip(0)
+	# '0.0.0.0'
+	int2ip(4294967295)
+	# '255.255.255.255'
 
 if __name__ == '__main__':
 	test()
