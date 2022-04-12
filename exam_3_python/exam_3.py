cfd8a16a8c7c75b885cf83a350dfc000b09a9aea
nyckolas <nyckolas.work@gmail.com>
2022-04-12 17:43:38 +0300

    Update exam.py

--- exam_3_python/exam_3.py
+++ exam_3_python/exam_3.py
@@ -232,14 +232,57 @@ def same_parity_filter(alist):
 	print(res)
 	return res
 
+def typecheck(func):
+	@wraps(func)
+	def inner(*args, **kwargs):
+		a = func.__annotations__ # Словарь с аннотацией переменных
+		#print(a) # {'x': <class 'int'>, 'y': <class 'str'>, 'z': <class 'float'>, 'foo': <class 'dict'>}
+		#print(a['x']) # <class 'int'>
+		#print(kwargs) # {'x': '4.0', 'y': [0]} - словарь с указанными переменными
+		for k in kwargs:
+			#print(k) # x - переменная
+			#print(kwargs[k]) # 4.0 - значение
+			if type(kwargs[k]) != a[k]: # проверяем тип переменной указанной и в аннотации
+				#print(f'Ошибка типа в переменной: {k} \nОжидается -> {a[k]} имеется -> {type(kwargs[k])}')
+				raise ValueError(f'Ошибка типа в переменной: {k} \nОжидается -> {a[k]} имеется -> {type(kwargs[k])}')
+			else:
+				return func(*args, **kwargs)
+	return inner
+
+
+def typecheck_all(func):	# Декоратор которыйпринимает текст и выводит на печать ф-цию
+	@wraps(func)	# Для того что бы ф-ция сохранила справку и имя используем @wraps(func)
+	def inner(*args, **kwargs):
+		a = func.__annotations__
+		try:
+			func(*args, **kwargs)
+		except ValueError as e:
+			print('Error: есть ошибки!')
+			for k in kwargs:
+				if type(kwargs[k]) != a[k]:
+					print(f'Ошибка типа в переменной: {k} \nОжидается -> {a[k]} имеется -> {type(kwargs[k])}')
+			print('Исправьте ошибки в переменных:')
+			print([k for k in kwargs if type(kwargs[k]) != a[k]])
+			return 
+	return inner
+
+
+
 def test():
+
+	@typecheck_all
+	@typecheck
+	def f(
+	x: int = 3,
+	y: str = "Hello!",
+	z: float = 3.0,
+	foo: dict = {'a': 42, 'b': 'bar'}
+	):
+		print(f'Ура в переменных нет ошибок!\nx = {x}, y = {y}, z = {z}, foo = {foo}')
+		print([k for k,v in locals().items()]) # ['x', 'y', 'z', 'foo'] - список всех локальных переменных
+		pass
 	
-	same_parity_filter([])
-	# []
-	same_parity_filter([2, 0, 1, -3, 10, -2])
-	# [2, 0, 10, -2]
-	same_parity_filter([-1, 0, 1, -3, 10, -2])
-	# [-1, 1, -3]
+	f(x=1, y = 'Привет, мир!')
 
 if __name__ == '__main__':
 	test()
