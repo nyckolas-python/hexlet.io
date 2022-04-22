8d4ce2618494e368d7f7dc1f1c40c6c8d5e78589
nyckolas <nyckolas.work@gmail.com>
2022-04-22 22:47:51 +0300

    generate_tree.py

---
+++ 11_tree/generate_tree.py
@@ -0,0 +1,83 @@
+from pprint import pprint
+
+
+def mkdir(name, children=[], meta={}):
+	dir = {
+		'name': name,
+		'type': 'directory',
+		'meta': meta,
+		'children': children
+	}
+	return dir
+
+def mkfile(name, meta={}):
+	file = {
+		'name': name,
+		'type': 'file',
+		'meta': meta
+	}
+	return file
+
+tree = mkdir('etc', [
+	mkfile('bashrc'),
+	mkdir('consul', [
+		mkfile('config.json'),
+	]),
+])
+
+new_tree = mkdir('python-package', [
+	mkfile('Makefile'),
+	mkfile('Readme.md'),
+	mkdir('dist'),
+	mkdir('tests', [
+		mkfile('test_solution.py'),
+	]),
+	mkfile('pyproject.toml'),
+	mkdir('.venv', [
+		mkdir('lib', [
+			mkdir('lib', [
+				mkdir('python3.6', [
+					mkdir('site-pachages', [
+						mkfile('hexlet-python-package.egg-link'),
+					]),
+				]),
+			]),
+		]),
+	], {'owner': 'root', 'hidden': False}),
+], {'hidden': True})
+
+pprint(new_tree, sort_dicts=False)
+
+'''
+{'name': 'python-package',
+ 'type': 'directory',
+ 'meta': {'hidden': True},
+ 'children': [{'name': 'Makefile', 'type': 'file', 'meta': {}},
+              {'name': 'Readme.md', 'type': 'file', 'meta': {}},
+              {'name': 'dist', 'type': 'directory', 'meta': {}, 'children': []},
+              {'name': 'tests',
+               'type': 'directory',
+               'meta': {},
+               'children': [{'name': 'test_solution.py',
+                             'type': 'file',
+                             'meta': {}}]},
+              {'name': 'pyproject.toml', 'type': 'file', 'meta': {}},
+              {'name': '.venv',
+               'type': 'directory',
+               'meta': {'owner': 'root', 'hidden': False},
+               'children': [{'name': 'lib',
+                             'type': 'directory',
+                             'meta': {},
+                             'children': [{'name': 'lib',
+                                           'type': 'directory',
+                                           'meta': {},
+                                           'children': [{'name': 'python3.6',
+                                                         'type': 'directory',
+                                                         'meta': {},
+                                                         'children': [{'name': 'site-pachages',
+                                                                       'type': 'directory',
+                                                                       'meta': {},
+                                                                       'children': [{'name': 'hexlet-python-package.egg-link',
+                                                                                     'type': 'file',
+                                                                                     'meta': {}}]}]}]}]}]}]}
+'''
