abdbc7ad6ab1dad6cf1a4663f97738769a6c8db4
nyckolas <nyckolas.work@gmail.com>
2022-04-25 18:21:05 +0300

    Update compress_images.py

--- 11_tree/compress_images.py
+++ 11_tree/compress_images.py
@@ -2,25 +2,21 @@ from pprint import pprint
 from generate_tree import is_directory, is_file, mkdir, mkfile, get_name, get_children, get_meta, is_directory, is_file
 from copy import deepcopy
 
-def compress_images(tree):
+def compress_images(node):
 	'''
 	Ф-ция принимает на фход папку или файл, находит все файлы картинки (.jpg) и сжимает их в 2 раза.
-	Возвращает обновленную директорию со сжатыми файлами и всеми остальными данными кторорые были нутри.
+	Возвращает обновленную директорию со сжатыми файлами и всеми остальными данными кторорые были внутри.
 	'''
-	def to_compress(node): # Ф-ция сжимает только файлы с расширением '.jpg'
-		name = get_name(node)
-		new_meta = deepcopy(get_meta(node))
-		if name[-4:] == '.jpg' and is_file(node): # условие оконочание '.jpg' и is_file True
+	name = get_name(node)
+	new_meta = deepcopy(get_meta(node))
+	if is_file(node): # условие оконочание '.jpg' и is_file True
+		if name[-4:] == '.jpg':
 			new_meta['size'] = int(new_meta.get('size')/2) # изменяем параментр ['size'] в словаре 'meta'
-			return mkfile(name, new_meta) # возвращает новый сжатый файл
-		return node
-	name = get_name(tree)
-	new_meta = deepcopy(get_meta(tree))
-	if is_directory(tree): # проверяем на входя файл или директория
-		children = get_children(tree)[:] # если директория, клонируем всю вложенную структуру файлов и папок
-		new_children = list(map(to_compress, children)) # сжимаем все файлы '.jpg'
-		return mkdir(name, new_children, new_meta) 
-	return to_compress(mkfile(name, new_meta))
+		return mkfile(name, new_meta) # возвращает новый сжатый файл
+	children = get_children(node) # если директория, клонируем всю вложенную структуру файлов и папок
+	new_children = list(map(lambda child: compress_images(child), children)) # сжимаем все файлы '.jpg'
+	new_tree = mkdir(name, new_children, new_meta)
+	return new_tree
 
 def test():
 # Test Program
@@ -31,7 +27,9 @@ def test():
 			mkfile('avatar.jpg', {'size': 100}),
 			mkfile('photo.jpg', {'size': 150}),
 			mkfile('test_name.jpeg', {'size': 150}),
-			mkdir('photo.jpg', [], {'hide': True})
+			mkdir('photo.jpg', [
+				mkfile('avatar_inside.jpg', {'size': 200})
+			], {'hide': True})
 		],
 		{'hide': False}
 	)
@@ -47,7 +45,10 @@ def test():
  'children': [{'name': 'avatar.jpg', 'type': 'file', 'meta': {'size': 50}},
               {'name': 'photo.jpg', 'type': 'file', 'meta': {'size': 75}},
               {'name': 'test_name.jpeg', 'type': 'file', 'meta': {'size': 150}},
-              {'name': 'photo.jpg', 'type': 'file', 'meta': {'hide': True}}]}
+              {'name': 'photo.jpg',
+               'type': 'directory',
+               'meta': {'hide': True},
+               'children': []}]}
 {'name': 'avatar.jpg', 'type': 'file', 'meta': {'size': 50}}
 '''
 
