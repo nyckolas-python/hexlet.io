b2ca69aa0eda974102aee6c5dfa5091f34616834
nyckolas <nyckolas.work@gmail.com>
2022-05-13 16:40:25 +0300

    Update flask_start.py

--- 14_web/flask_start.py
+++ 14_web/flask_start.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+menu = ["Установка", "Первое приложение", "Обратная связь"]
+
 @app.route('/')
-def hello_world():
-	return 'Hello, World!\nHi, Nyckolas!'
+def index():
+	return render_template('index.html', title='Про Flask', menu=menu)
+
+@app.route('/about')
+def about():
+	return render_template('about.html', title='О сайте')
+
+if __name__ == "__main__":
+	app.run(debug=True)
