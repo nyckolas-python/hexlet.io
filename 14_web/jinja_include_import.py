c91d67bfbc3074d9d4b7afcbc47fe5ae061f5b0c
nyckolas <nyckolas.work@gmail.com>
2022-05-16 12:42:35 +0300

    Add jinja_include_import.py

---
+++ 14_web/jinja_include_import.py
@@ -0,0 +1,31 @@
+from jinja2 import Environment, FileSystemLoader
+
+'''
+https://jinja.palletsprojects.com/en/2.11.x/api/#jinja2.Environment
+
+https://jinja.palletsprojects.com/en/2.11.x/api/#jinja2.FileSystemLoader
+Загрузчики:
+
+PackageLoader - для загрузки пакетов из шаблонов
+DictLoader - для загрузки пакетов из словаря
+FunctionLoader - для загрузки пакетов на основе фунции
+PrefixLoader - загрузчик, использующий словарь для построения подкаталогов 
+ChiceLoader - загрузчик, содержащий список других загрухчиков(если один не сработает,
+выбирает следующий и т.д.)
+ModuleLoader - загрузчик для скомпилированных шаблонов 
+'''
+
+persons = [
+	{'name': 'Adam', 'age': 37, 'weight': 74},
+	{'name': 'Nyckolas', 'age': 32, 'weight': 64},
+	{'name': 'Michel', 'age': 42, 'weight': 84},
+	{'name': 'Clarck', 'age': 47, 'weight': 94}
+]
+
+file_loader = FileSystemLoader('14_web/templates/')
+env = Environment(loader=file_loader)
+
+tm = env.get_template('page.tpl')
+msg = tm.render(users = persons, domain='http://localhost:5000', title='Jinja use include and import')
+
+print(msg)
