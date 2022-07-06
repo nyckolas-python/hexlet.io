9b174ff07e2a63dadb020673fe3ecc9b8525a127
nyckolas <nyckolas.work@gmail.com>
2022-07-06 21:52:16 +0300

    Update Counter.py

--- 18_oop/Counter.py
+++ 18_oop/Counter.py
@@ -1,26 +1,31 @@
+#!/usr/bin/env python3
 class Counter:
-    value = int(0)
+	value = int(0)
 # Именно через аргумент self метод получает доступ к атрибутам связанного объекта 
 # (и его класса, конечно же).
-    def inc(self, delta=1):
-        if self.value + delta >= 0:
-            self.value += delta 
-        else: self.value = 0
-        return self.value
-    
-    def dec(self, delta=1):
-        if self.value - delta >= 0:
-            self.value -= delta 
-        else: self.value = 0
-        return self.value
+	def inc(self, delta=1):
+		if self.value + delta >= 0:
+			self.value += delta 
+		else: self.value = 0
+		return self.value
+	
+	def dec(self, delta=1):
+		if self.value - delta >= 0:
+			self.value -= delta 
+		else: self.value = 0
+		return self.value
 
-c = Counter()
-c.inc()
-c.inc()
-c.inc(40)
-print(c.value) # 42
-c.dec()
-c.dec(30)
-print(c.value) # 11
-c.dec(delta=100)
-print(c.value) # 0
+def test():
+	c = Counter()
+	c.inc()
+	c.inc()
+	c.inc(40)
+	print(c.value) # 42
+	c.dec()
+	c.dec(30)
+	print(c.value) # 11
+	c.dec(delta=100)
+	print(c.value) # 0
+
+if __name__ == '__main__':
+	test()
