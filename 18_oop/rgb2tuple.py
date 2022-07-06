ca17513086bccad98e6654c58bf09670d1384d97
nyckolas <nyckolas.work@gmail.com>
2022-07-06 21:52:54 +0300

    Update rgb2tuple.py

--- 18_oop/rgb2tuple.py
+++ 18_oop/rgb2tuple.py
@@ -1,7 +1,7 @@
 class RGB:
-    red = 0
-    green = 0
-    blue = 0
+	red = 0
+	green = 0
+	blue = 0
 
 red,green,blue = RGB(), RGB(), RGB()
 red.red = 255
@@ -9,8 +9,8 @@ blue.blue = 255
 green.green = 255
 
 def rgb2tuple(rgb):
-    if isinstance(rgb, RGB):
-        return rgb.red, rgb.green, rgb.blue
+	if isinstance(rgb, RGB):
+		return rgb.red, rgb.green, rgb.blue
 
 print(rgb2tuple(red))
 # (255, 0, 0)
